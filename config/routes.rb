372ee68338e6012a82b0d3d4931088511450cc88
marvinick <marviluw@gmail.com>
2015-03-13 15:13:19 -0500

    added users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
 
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
